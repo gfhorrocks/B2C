8ded6e44eba0f8a7902cc9aee5de86af5f8c5f33
GregH <43968370+gfhorrocks@users.noreply.github.com>
2019-05-30 08:07:04 -0400

    Initial Push - New Customer -> Database Working

---
+++ client/src/router.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/vendor',
+      name: 'vendor',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Vendor.vue')
+    },{
+      path: '/customer',
+      name: 'customer',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Customer.vue')
+    },{
+      path: '/newvendor',
+      name: 'newvendor',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/NewVendor.vue')
+    },{
+      path: '/newcustomer',
+      name: 'newcustomer',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/NewCustomer.vue')
+    }
+  ]
+})
---
+++ client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <div class="home">
+   
+  </div>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import test from '@/components/test.vue'
+
+export default {
+  name: 'home',
+  components: {
+    test
+  }
+}
+</script>
